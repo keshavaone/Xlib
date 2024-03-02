fe3092518f62e8c78016a65ee24a3c8af114cd3f
Xlib <aman9893089064@gmail.com>
2024-03-02 17:46:45 +0530

    Updated personal and professional Details

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -5,14 +5,14 @@ export default function Meta() {
     return (
         <Head>
            /* Primary Meta Tags */
-            <title>Aman Agrawal Portfolio - Computer Engineering Student</title>
+            <title>Aman Agrawal's Portfolio - Xlib</title>
             <meta charSet="utf-8" />
-            <meta name="title" content="Aman Agrawal Portfolio - Computer Engineering Student" />
+            <meta name="title" content="Aman Agrawal's Portfolio - Xlib" />
             <meta name="description"
                 content="Aman Agrawal's (xlibraries) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="author" content="Aman Agrawal (xlibraries)" />
             <meta name="keywords"
-                content="xlibraries, xlibraries's portfolio, xlibraries linux, ubuntu portfolio, aman agrawal portfolio, xlib, aman agrawal, aman ubuntu, aman agrawal ubuntu portfolio" />
+                content="xlibraries, xlibraries's portfolio, xlibraries linux, ubuntu portfolio, Aman Agrawal's Portfolio, xlib, aman agrawal, aman ubuntu, aman agrawal ubuntu portfolio" />
             <meta name="robots" content="index, follow" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="language" content="English" />
@@ -22,20 +22,20 @@ export default function Meta() {
             /* Search Engine */
             <meta name="image" content="images/logos/fevicon.png" />
             /* Schema.org for Google */
-            <meta itemProp="name" content="Aman Agrawal Portfolio - Computer Engineering Student" />
+            <meta itemProp="name" content="Aman Agrawal's Portfolio - Xlib" />
             <meta itemProp="description"
                 content="Aman Agrawal's (xlibraries) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta itemProp="image" content="images/logos/fevicon.png" />
             /* Twitter */
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content="Aman Agrawal Portfolio - Computer Engineering Student" />
+            <meta name="twitter:title" content="Aman Agrawal's Portfolio - Xlib" />
             <meta name="twitter:description"
                 content="Aman Agrawal's (xlibraries) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="twitter:site" content="xlibraries" />
             <meta name="twitter:creator" content="xlibraries" />
             <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
             /* Open Graph general (Facebook, Pinterest & Google+) */
-            <meta name="og:title" content="Aman Agrawal Portfolio - Computer Engineering Student" />
+            <meta name="og:title" content="Aman Agrawal's Portfolio - Xlib" />
             <meta name="og:description"
                 content="Aman Agrawal's (xlibraries) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="og:image" content="images/logos/logo_1200.png" />
