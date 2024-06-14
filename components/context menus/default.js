46dfc1866d07db4c69a2c1d38c577e6e34ed26be
Keshava <keshava_one@outlook.com>
2024-06-14 13:04:35 +0530

    component changes applied

--- components/context menus/default.js
+++ components/context menus/default.js
@@ -10,13 +10,13 @@ function DefaultMenu(props) {
                 <span className="ml-5">❗</span> <span className="ml-2">Report bugs</span>
             </a>
             <Devider />
-            <a rel="noreferrer noopener" href="https://www.linkedin.com/in/xlib/" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="https://www.linkedin.com/in/rao-keshava/" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">🙋‍♂️</span> <span className="ml-2">Follow on <strong>Linkedin</strong></span>
             </a>
-            <a rel="noreferrer noopener" href="https://github.com/xlibraries/" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="https://github.com/keshavaone/" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">🤝</span> <span className="ml-2">Follow on <strong>Github</strong></span>
             </a>
-            <a rel="noreferrer noopener" href="mailto:aman9893089064@gmail.com" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="mailto:career_keshava@outlook.com" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">📥</span> <span className="ml-2">Contact Me</span>
             </a>
             <Devider />
